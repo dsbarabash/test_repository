7c4ac94f97700f6a1776cc4469635913d8bbdcb8
Daria <dary.barabash@yandex.ru>
2025-01-29 11:08:17 +0300

    add create chess func

--- main.go
+++ main.go
@@ -2,7 +2,28 @@ package main
 
 import "fmt"
 
+func createChess(n int) string {
+	chess := ""
+	for j := 0; j < n; j++ {
+		for i := 0; i < n; i++ {
+			if i%2 == 0 && j%2 == 0 {
+				chess = chess + " "
+			} else if i%2 == 0 && j%2 != 0 {
+				chess = chess + "#"
+			} else if i%2 == 1 && j%2 != 0 {
+				chess = chess + " "
+			} else if i%2 == 1 && j%2 != 1 {
+				chess = chess + "#"
+			}
+
+		}
+		chess = chess + "\n"
+	}
+	return chess
+}
+
 func main() {
 	fmt.Println("Hello Go!")
-	fmt.Println("Hello World!")
+	chess := createChess(8)
+	fmt.Println(chess)
 }
